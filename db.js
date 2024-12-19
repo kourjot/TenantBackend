6018d52aef543c69e54478b69a7308e197b4ae28
kourjot <pkour6188@gmail.com>
2024-12-19 13:37:06 +0530

    thirdcomment

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import {connect} from "mongoose"
+import "dotenv/config";
 const db =async()=>{
     try{
-        await connect(process.env.MONGO_URL,)
+        await connect("mongodb+srv://pkour6188:RhFrZzxOKU47YyoK@tenantdbs.ivn6n.mongodb.net/?retryWrites=true&w=majority&appName=tenantdbs")
         console.log('Connected to MongoDB!')
     }catch(e){
         console.error('Failed to connect to MongoDB:', e.message)
