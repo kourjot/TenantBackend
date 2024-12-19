79d82ff89516ab0209cdc3e5ea30aeb363626cc4
kourjot <pkour6188@gmail.com>
2024-12-19 11:59:31 +0530

    second change

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import db from "./db.js";
 import { router } from "./router/router.js";
-import cors from "cors";
+// import cors from "cors";
 import "dotenv/config";
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({extended:true}))
+// app.use(cors());
 app.use("/api/auth", router);
 db();
 app.listen(process.env.PORT, async () => {
